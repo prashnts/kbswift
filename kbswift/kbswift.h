387f4afb157cd774e7d0cc9e7acf0129aeab56d3
Prashant Sinha <prashant+git@noop.pw>
2017-01-04 23:02:53 +0530

    Initial Commit

---
+++ kbswift/kbswift.h
@@ -0,0 +1,19 @@
+//
+//  kbswift.h
+//  kbswift
+//
+//  Created by Prashant Sinha on 04/01/17.
+//  Copyright © 2017 Noop. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for kbswift.
+FOUNDATION_EXPORT double kbswiftVersionNumber;
+
+//! Project version string for kbswift.
+FOUNDATION_EXPORT const unsigned char kbswiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kbswift/PublicHeader.h>
+
+
