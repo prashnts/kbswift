c2120e862d67a9b229baabd61c9439d549d1b941
Prashant Sinha <prashant+git@noop.pw>
2017-01-09 00:12:07 +0530

    Update docstrings

--- kbswift/Crypto.swift
+++ kbswift/Crypto.swift
@@ -18,28 +18,36 @@ let SEED_LEN = 32,
     PRIKEY_LEN = 64,
     SIGNATURE_LEN = 64
 
-enum CryptoError: Error {
+public enum CryptoError: Error {
     case invalidParam
     case seedLengthMismatch
 }
 
-typealias CryptoBuffer = Array<UInt8>
-typealias CryptoKeyPair = (public_key: CryptoBuffer, private_key: CryptoBuffer)
+public typealias CryptoBuffer = Array<UInt8>
+public typealias CryptoKeyPair = (public_key: CryptoBuffer, private_key: CryptoBuffer)
 
 typealias _CryptoBufferPtr = UnsafeMutablePointer<UInt8>
 
-class Crypto {
-    /// Scrypt is a password-based key derivation function.
-    /// This method wraps scrypt implementation in libscrypt.
-    /// 
-    /// - Parameters
-    ///     - pswd: Password
-    ///     - salt: Salt
-    ///     - N: CPU AND RAM cost (first modifier)
-    ///     - r: RAM Cost
-    ///     - p: CPU cost (parallelisation)
-    ///     - bufflen: Intended length of output buffer.
-    /// - Returns: Key
+extension Array where Element: UInt8 {
+    var base64: String {
+        return Data(self).base64EncodedString()
+    }
+}
+
+public class Crypto {
+    /**
+     Scrypt is a password-based key derivation function.
+     This method wraps scrypt implementation in libscrypt.
+     
+     - Parameters
+        - pswd: Password
+        - salt: Salt
+        - N: CPU AND RAM cost (first modifier)
+        - r: RAM Cost
+        - p: CPU cost (parallelisation)
+        - bufflen: Intended length of output buffer.
+     - Returns: Key
+     */
     public static func scrypt(pswd: String, salt: String, N: UInt64, r: UInt32, p: UInt32, bufflen: Int) throws -> CryptoBuffer? {
         let sane = N >> 2 > 0 && r * p < MODIFIER_BOUND && bufflen < BUFFER_LEN_MAX
 
@@ -65,7 +73,10 @@ class Crypto {
 }
 
 
-class EdDSA {
+public class EdDSA {
+    /**
+     Returns a 32 Byte Random Seed for Key Generation.
+     */
     public static func createSeed() -> CryptoBuffer {
         let seed_buffer = _CryptoBufferPtr.allocate(capacity: SEED_LEN)
 
@@ -76,6 +87,13 @@ class EdDSA {
         return seed
     }
 
+    /**
+     Returns a new key pair from the given seed.
+     
+     - Parameters:
+        - seed: A 32 Byte CryptoBuffer
+     - Returns: A Public and Private KeyPair
+     */
     public static func createKeypair(seed: CryptoBuffer) throws -> CryptoKeyPair {
         if seed.count != SEED_LEN {
             throw CryptoError.seedLengthMismatch
@@ -96,6 +114,14 @@ class EdDSA {
         return (pub_key, private_key)
     }
 
+    /**
+     Returns a signature of the given message with the given key pair.
+     
+     - Parameters:
+        - message: Message to be signed
+        - keypair: A Public, Private KeyPair
+     - Returns: A 64 Byte Signature
+     */
     public static func sign(message: String, keypair: CryptoKeyPair) -> CryptoBuffer {
         let sig_buffer = _CryptoBufferPtr.allocate(capacity: SIGNATURE_LEN)
         Ed25519.ed25519_sign(sig_buffer, message, message.characters.count, keypair.public_key, keypair.private_key)
@@ -105,6 +131,9 @@ class EdDSA {
         return signature
     }
 
+    /**
+     Verifies the signature on the given message using the given Public Key.
+     */
     public static func verify(signature: CryptoBuffer, message: String, public_key: CryptoBuffer) -> Bool {
         let match = Ed25519.ed25519_verify(signature, message, message.characters.count, public_key)
         return match == 1
